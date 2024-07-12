c4414792a72fa964016412dc3e9fadde75f7dc82
Hadi Abdul <hadiabdul@Hadis-Laptop.local>
2024-07-12 02:20:50 -0500

    updated next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: process.env.NODE_ENV === 'production' ? '/{datebetter}' : '',
-    assetPrefix: process.env.NODE_ENV === 'production' ? '/{datebetter}/' : '',
+    basePath: process.env.NODE_ENV === 'production' ? '/datebetter' : '',
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/datebetter/' : '',
 }
 
 module.exports = nextConfig
